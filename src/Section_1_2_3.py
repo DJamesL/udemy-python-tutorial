98fd9f5587c6f493aec0796cb18573a0afaff2a5
jamesdavid <james.david@artesyn.com>
2017-11-18 22:45:36 +0800

    initial commit for section_1_2_3

---
+++ src/Section_1_2_3.py
@@ -0,0 +1,20 @@
+#string manipulation
+
+int("5") #converts "5" string to an integer
+str(6) #converts integer 6 to a string
+
+print("String manipulation tutorial")
+
+"5" + "6" #concatenates 5 & 6, rersult "56"
+
+
+
+"Hello:Nick:World".split(":")  #splits a string using the ":" character
+#Returns an array of strings
+#['Hello', 'Nick', 'World']
+
+
+#boolean
+
+ 5 is 5 # will return true
+ 5 is not 5 # will return false
